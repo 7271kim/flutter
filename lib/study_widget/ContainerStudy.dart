bfd10fabdeb00c1746aa0272aa72284b98eb4074
김석진 <7271kim@naver.com>
2022-07-13 02:00:08 +0900

    [Flutter] Container 추가

--- lib/study_widget/ContainerStudy.dart
+++ lib/study_widget/ContainerStudy.dart
@@ -11,14 +11,23 @@ class _ContainerStudyState extends State<ContainerStudy> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.only(bottom: 10, left: 10, right: 8, top: 20),
+      margin: EdgeInsets.all(1),
+      padding: EdgeInsets.only(bottom: 1, left: 1, right: 1, top: 1),
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.center,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [Colors.blue[300]!, Colors.blue[900]!],
         ),
+        shape: BoxShape.circle,
+      ),
+      child: Icon(
+        Icons.search,
+        color: Colors.white,
+        size: 100,
       ),
     );
   }
