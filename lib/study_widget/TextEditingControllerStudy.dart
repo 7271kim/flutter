c5b15ba573c16a2741eb95a38d6149c230583542
seokjin <7271kim@naver.com>
2022-07-17 15:41:09 +0900

    [Flutter] TexteditorController

--- lib/study_widget/TextEditingControllerStudy.dart
+++ lib/study_widget/TextEditingControllerStudy.dart
@@ -12,24 +12,45 @@ class _TextEditingControllerStudyState
     extends State<TextEditingControllerStudy> {
   // create text controller
   TextEditingController createTextController = TextEditingController();
+  TextEditingController createTextController2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: createTextController,
-      autofocus: true,
-      // 커서 색상
-      cursorColor: Colors.indigo,
-      decoration: InputDecoration(
-        hintText: "한 줄 일기를 작성해주세요.",
-        // 포커스 되었을 때 밑줄 색상
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.indigo),
+    return Column(
+      children: [
+        TextField(
+          controller: createTextController,
+          autofocus: true,
+          // 커서 색상
+          cursorColor: Colors.indigo,
+          decoration: InputDecoration(
+            hintText: "한 줄 일기를 작성해주세요.",
+            // 포커스 되었을 때 밑줄 색상
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.indigo),
+            ),
+          ),
+          onSubmitted: (_) {
+            createTextController2.text = "zzzzz";
+          },
         ),
-      ),
-      onSubmitted: (_) {
-        Navigator.pop(context);
-      },
+        TextField(
+          controller: createTextController2,
+          autofocus: true,
+          // 커서 색상
+          cursorColor: Colors.indigo,
+          decoration: InputDecoration(
+            hintText: "한 줄 일기를 작성해주세요.22",
+            // 포커스 되었을 때 밑줄 색상
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.indigo),
+            ),
+          ),
+          onSubmitted: (_) {
+            createTextController.text = "222";
+          },
+        )
+      ],
     );
   }
 }
